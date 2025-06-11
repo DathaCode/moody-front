8f0d7ff1408e03d7163de15a69622bf3e038b9f5
Vidath Dulanjith <vdulanjith@gmail.com>
2025-06-12 01:21:41 +0530

    tailwind configuration done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+    "./index.html",
   ],
   theme: {
     container: {
